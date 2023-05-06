bacd3f705edce0386fecba338e6681685b32bb40
Ron Heller <ron.heller@work-microwave.com>
2023-05-06 05:56:23 +0200

    add generic point

---
+++ src/point.rs
@@ -0,0 +1,90 @@
+#![allow(dead_code)]
+
+use std::ops::{Add,Sub,Mul,Div,Index};
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
+}
+
+pub trait Abs {
+    type Output;
+    fn abs(self) -> Self::Output;
+}
+
+impl Abs for Point<f64> {
+    type Output = Self;
+    fn abs(self) -> Self::Output {
+        Self {
+            x: self.x.abs(),
+            y: self.y.abs(),
+        }
+    }
+}
+
+impl <T>Add for Point<T> 
+where T: Add<Output = T>{
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl <T>Sub for Point<T> 
+where T: Sub<Output = T>{
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl <T>Mul for Point<T> 
+where T: Mul<Output = T>{
+    type Output = Self;
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        }
+    }
+}
+
+impl <T>Div for Point<T> 
+where T: Div<Output = T>{
+    type Output = Self;
+    fn div(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        }
+    }
+}
+
+impl <T>Index<usize> for Point<T> {
+    type Output = T;
+    fn index(&self, i: usize) -> &Self::Output {
+        match i {
+            0 => &self.x,
+            1 => &self.y,
+            _ => panic!("Add more index values in the index function, if dimension is increased!"),
+        }
+    }
+}
+
+impl<T> Point<T> 
+where T: PartialOrd {
+    pub fn index_of_significance(&self) -> usize {
+        if self.x > self.y {
+            return 0;
+        }
+        return 1;
+    }
+}
+

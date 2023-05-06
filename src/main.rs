bdbc3d1c182f8205a5ec27d90ca4ae6fea141b8e
Ron Heller <ron.heller@work-microwave.com>
2023-05-06 05:57:04 +0200

    write main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+#![allow(dead_code)]
+
+mod point;
+mod line;
+use line::{LineVec, LineRelation};
+
+use std::time::Instant;
+use indicatif::{ProgressBar, ProgressStyle};
+
 fn main() {
-    println!("Hello, world!");
+    println!("The progress speed will increase over time!");
+    let line_vec = LineVec::from_file("s_10000_1.dat");
+
+    let bar = ProgressBar::new(line_vec.rows as u64);
+    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7}[{percent}%] {msg} Estimated: {eta}")
+        .unwrap()
+        .progress_chars("##-"));
+
+    let mut intersections = 0;
+    let mut colinear_overlaps = 0;
+
+    let now = Instant::now();
+
+    for (index1, line1) in line_vec.data.iter().enumerate() {
+        for line2 in line_vec.data.iter().skip(index1 + 1) {
+            match line1.intersect(line2) {
+                LineRelation::Intersecting => intersections += 1,
+                LineRelation::NonIntersecting => (),
+                LineRelation::ColinearOverlap => colinear_overlaps += 1,
+                LineRelation::ColinearNonOverlap => (),
+            }
+        }
+        bar.inc(1);
+    }
+    bar.finish();
+    println!(
+        "\nIntersecting lines: {}\nColinear & overlapping lines: {}\nDone in: {}ms",
+        intersections,
+        colinear_overlaps,
+        now.elapsed().as_millis()
+    );
+
 }
+
+#[cfg(test)]
+mod test;

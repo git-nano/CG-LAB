56d49231c7807beb11b267555cbcf5ab69b0a0f8
Ron Heller <ron.heller@work-microwave.com>
2023-07-14 04:03:54 +0200

    for others probably easier with python3

--- lab3/setup-venv.sh
+++ lab3/setup-venv.sh
@@ -3,14 +3,14 @@ set -eu
 set -e pipefail
 
 printf "Creating project-specific virtualenv in .venv..."
-python3.11 -m venv .venv
+python3 -m venv .venv
 printf "OK\n"
 
 source .venv/bin/activate
 
 printf "Installing development requirements..."
-python3.11 -m pip install --upgrade pip --quiet
-python3.11 -m pip install --quiet -r requirements.txt --upgrade
+python3 -m pip install --upgrade pip --quiet
+python3 -m pip install --quiet -r requirements.txt --upgrade
 
 printf "venv initialized!\n"
 printf "To activate it, run:\n\n"
